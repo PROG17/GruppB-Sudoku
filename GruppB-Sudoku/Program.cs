ab1614980f351468b76dcb2e5ba4339414e7aa6b
Feyona N. Thylander <Feyona.NaluzziThylander@yh.nackademin.se>
2017-09-15 10:17:05 +0200

    Rensa test text

--- GruppB-Sudoku/Program.cs
+++ GruppB-Sudoku/Program.cs
@@ -11,21 +11,7 @@ namespace GruppB_Sudoku
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Micke Testar ifall det funkar som det ska");
-
-            int a = 2;
-            int b = 10;
-
-            if (a < b)
-            {
-               Console.WriteLine("Welcome to the Matrix Grupp B...");
-            }
-
-            
-
-            Console.ReadLine();
-
-
+         
         }
     }
 }
