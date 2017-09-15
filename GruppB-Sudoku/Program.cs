84375fa128a4015cefe541219e9c573a7b99b126
Adrian Kuryatko-Mihai <31843562+AdrianKuryatko@users.noreply.github.com>
2017-09-15 11:01:17 +0200

    copy unsolvedsodoku.txt to /bin/debug

--- GruppB-Sudoku/Program.cs
+++ GruppB-Sudoku/Program.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GruppB_Sudoku
-{
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            string filename = @"C:\Users\hobbo\Documents\Program C#\Projects\GruppB-Sudoku\GruppB-Sudoku\unsolvedsodoku.txt";
+            string filename = Environment.CurrentDirectory + @"\unsolvedsodoku.txt";
             var sodoku = new SodokuGame(filename);
         }
     }
